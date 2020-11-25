50618687db97e0348bcd06f60d100c27978ce3af
takdongwan <tak46123@gmail.com>
2020-11-25 21:54:35 +0900

    Express Server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("test");``    
+const express = require("express");
+const app = express(); //app변수를 선언해서 express를 실행시킴 
+
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
