5b1d31ca8c8ac2818e3ebe4df3d6d3a8e8609590
takdongwan <tak46123@gmail.com>
2020-11-27 03:01:57 +0900

    nodeJS using babel

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const express = require("express");
+import express from "express";
 const app = express(); //app변수를 선언해서 express를 실행시킴 
-
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
 
-function handleHome(req, res) {
-  res.send("Hello from home");
-}
 
-function handleProfile(req, res) {
-  res.send("You are on my profile");
-}
+
+const handleHome = (req, res) => res.send("Hello from my ass");
+
+const betweenHome = (req, res, next) => {
+  console.log("Between");
+  next();
+};
+
+app.use(betweenHome);
+
+
+const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.get("/", handleHome);
-
 app.get("/profile", handleProfile);
-
 app.listen(PORT, handleListening);
