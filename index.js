a31d205640d5692e4eee5b4c9e5af122ad7265f8
takdongwan <dongwantak@gmail.com>
2020-11-28 02:42:53 +0900

    Express core: middleware

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express(); //app변수를 선언해서 express를 실행시킴 
 
 const PORT = 4000;
@@ -10,16 +14,15 @@ const handleListening = () =>
 
 const handleHome = (req, res) => res.send("Hello from my ass");
 
-const betweenHome = (req, res, next) => {
-  console.log("Between");
-  next();
-};
-
-app.use(betweenHome);
-
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 app.listen(PORT, handleListening);
