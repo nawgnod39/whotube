0ea7a8eb22fc0863c5ec8db37f7b7207dcd8326f
takdongwan <dongwantak@gmail.com>
2020-11-29 22:53:50 +0900

    MVC Pattern

--- app.js
+++ app.js
@@ -3,15 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express(); //app변수를 선언해서 express를 실행시킴 
-
-
-const handleHome = (req, res) => res.send("Hello from my ass");
-
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 
 app.use(cookieParser());
@@ -19,8 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
+app.use("/", globalRouter);
 app.use("/user", userRouter);
-
+app.use("/video", videoRouter);
 export default app;
