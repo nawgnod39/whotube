b292abaf69af3d611b48bc12617e138c1cd90d68
takdongwan <dongwantak@gmail.com>
2020-12-13 21:29:07 +0900

    Uploading and Creating a Video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -6,12 +6,16 @@ import {
     videoDetail,
     editVideo,
     deleteVideo
-  } from "../controllers/videoController";
+} from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
+
+
+
 const videoRouter = express.Router();
 
 
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
