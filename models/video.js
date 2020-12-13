cb221c60400fbf5fc81fd6a9648c311b6f2a8ece
takdongwan <dongwantak@gmail.com>
2020-12-13 14:55:04 +0900

    Comment Model

--- models/video.js
+++ models/video.js
@@ -13,10 +13,12 @@ const VideoSchema = new mongoose.Schema({
   views: {
     type: Number,
     default: 0
+    //처음 video가 생성되면 views 를 0이 되게 함.
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now//현재날짜 반환 함수
+    //내가 document를 저장할때 마다 해당 function을 실행해서 현재 날짜를 받음.
   },
   comments: [
     {
@@ -26,5 +28,5 @@ const VideoSchema = new mongoose.Schema({
   ]
 });
 
-const model = mongoose.model("Video", VideoSchema);
+const model = mongoose.model("Video", VideoSchema);//스키마를 이용해 model을 만들어보면  definition을 통해 실제 document를 만드는 것이고 스키마가 데피니션 같은것임.
 export default model;
